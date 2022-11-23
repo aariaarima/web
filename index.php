21049d88ca9afab7fe34ad348b4c09ce909eb864
aarima <108648547+aariaarima@users.noreply.github.com>
2022-11-23 11:54:16 +0545

    Php upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//This is a variable.
+#This is a variable.
+$a="<h1>This is a variable</h1>";
+$x="This is a variable x";
+$y="This is a variable y";
+/*This is first line
+This is second line*/
+echo $a;
+echo $x . "." .$y;
+echo "Hello World!";
+?>
