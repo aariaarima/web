4a31edccbc074047e8bb19b42342256081fb91a6
aarima <108648547+aariaarima@users.noreply.github.com>
2022-11-25 10:28:51 +0545

    PHP Switch Case and Loop

---
+++ loop.php
@@ -0,0 +1,36 @@
+<?php
+//PHP Loops (For Loop)
+//To print even numbers except 60
+for ($x = 0; $x <= 100; $x+=2) {
+    if ($x == 60){
+        continue;
+    }
+    echo "The number is $x. <br>";
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ num.php
@@ -0,0 +1,16 @@
+<?php
+//PHP switch 
+$num =3;
+
+switch ($num) {
+  case "1":
+    echo "Your favorite num is 1";
+    break;
+  case "2":
+    echo "Your favorite num is 2";
+    break;
+  case "3":
+    echo "Your favorite num is 3";
+    break;
+  }
+?>
